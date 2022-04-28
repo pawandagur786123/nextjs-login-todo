a81fa3e19ae5bbd70ce58e54ae95e290b635882f
Pawan Dagur <pawan.dagur@innovationm.com>
2022-04-28 11:55:59 +0530

    removed commented code

--- store/index.js
+++ store/index.js
@@ -1,24 +1,6 @@
-// import { createStore, combineReducers, applyMiddleware } from "redux";
-// import { composeWithDevTools } from "redux-devtools-extension";
-// import notesReducer from "./notesReducer";
-// import authReducer from "./authreducer";
-// import editReducer from "./editreducer"
-
-
-// const rootReducer = combineReducers({
-//     auth : authReducer,
-//     notes: notesReducer,
-//     editing: editReducer
-// });
-
-
-// export const initializeStore = () => {
-//   return createStore(rootReducer, composeWithDevTools(applyMiddleware()));
-// };
-
 import { createStore, combineReducers, applyMiddleware } from 'redux'
 import { persistStore, persistReducer } from 'redux-persist'
-import storage from 'redux-persist/lib/storage' // defaults to localStorage for web
+import storage from 'redux-persist/lib/storage'
 import { composeWithDevTools } from "redux-devtools-extension";
 import notesReducer from "./notesReducer";
 import authReducer from "./authreducer";
